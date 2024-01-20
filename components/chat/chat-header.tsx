a0081d4d36ff3cac1f4a5f2a1ca0ce41b00084c3
墨綠BG <s5460703@gmail.com>
2024-01-20 13:04:21 +0800

    ✨ feat：對話模組初始化

--- components/chat/chat-header.tsx
+++ components/chat/chat-header.tsx
@@ -1,7 +1,7 @@
 import { Hash } from "lucide-react";
 
 import { MobileToggle } from "@/components/mobile-toggle";
-// import { UserAvatar } from "@/components/user-avatar";
+import { UserAvatar } from "@/components/user-avatar";
 // import { SocketIndicator } from "@/components/socket-indicator";
 
 // import { ChatVideoButton } from "./chat-video-button";
@@ -25,10 +25,9 @@ export const ChatHeader = ({
       {type === "channel" && (
         <Hash className="w-5 h-5 text-zinc-500 dark:text-zinc-400 mr-2" />
       )}
-      {/*
       {type === "conversation" && (
         <UserAvatar src={imageUrl} className="h-8 w-8 md:h-8 md:w-8 mr-2" />
-      )}*/}
+      )}
       <p className="font-semibold text-md text-black dark:text-white">{name}</p>
       {/* <div className="ml-auto flex items-center">
         {type === "conversation" && <ChatVideoButton />}
