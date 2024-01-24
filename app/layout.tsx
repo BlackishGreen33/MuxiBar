f22ca2d78ce08a428243196caf1fe8e9da7b197c
墨綠BG <s5460703@gmail.com>
2024-01-24 22:38:57 +0800

    ✨ feat：新增chat messages組件和chat item組件

--- app/layout.tsx
+++ app/layout.tsx
@@ -6,6 +6,7 @@ import { cn } from "@/lib/utils";
 import { ThemeProvider } from "@/components/providers/theme-provider";
 import { ModalProvider } from "@/components/providers/modal-provider";
 import { SocketProvider } from "@/components/providers/socket-provider";
+import { QueryProvider } from "@/components/providers/query-provider";
 import "./globals.css";
 
 const font = Open_Sans({ subsets: ["latin"] });
@@ -32,7 +33,7 @@ export default function RootLayout({
           >
             <SocketProvider>
               <ModalProvider />
-              {children}
+              <QueryProvider>{children}</QueryProvider>
             </SocketProvider>
           </ThemeProvider>
         </body>
